d13b599a9ef6a11f4e83681dc6ecdf3704192a59
Eugene Nezhuta <theneiam@gmail.com>
2015-03-23 16:27:40 +0200

    Old name fixed on tests

--- test/test.js
+++ test/test.js
@@ -1,30 +1,30 @@
 
 var assert  = require('assert'),
     request = require('supertest'),
-    prowl   = require('..');
+    blurr   = require('..');
 
-describe('prowl() test suits', function() {
+describe('blurr() test suits', function() {
 
     describe('Basic operations', function() {
 
         it('should require config', function() {
-            assert.throws(prowl.bind(), /configuration required/);
+            assert.throws(blurr.bind(), /configuration required/);
         });
 
         it('should require configuration as object', function() {
-            assert.throws(prowl.bind(prowl, 'string'), /configuration should be an object/);
+            assert.throws(blurr.bind(blurr, 'string'), /configuration should be an object/);
         });
 
         it('should require config paths', function() {
-            assert.throws(prowl.bind(prowl, {}), /configuration paths required/);
+            assert.throws(blurr.bind(blurr, {}), /configuration paths required/);
         });
 
         it('should require config paths controllers', function() {
-            assert.throws(prowl.bind(prowl, { paths: {} }), /configuration paths controllers required/);
+            assert.throws(blurr.bind(blurr, { paths: {} }), /configuration paths controllers required/);
         });
 
         it('should require config resources', function() {
-            assert.throws(prowl.bind(prowl, { paths: {} }), /configuration paths controllers required/);
+            assert.throws(blurr.bind(blurr, { paths: {} }), /configuration paths controllers required/);
         });
 
     });
